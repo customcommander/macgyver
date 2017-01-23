ab4e9546e03e8c6ce46f2122ad61600a3b118e38
customcommander <hello@spinjs.com>
2017-01-23 21:58:06 +0000

    Releasing MacGyver 1.1.1

--- dist/macgyver.js
+++ dist/macgyver.js
@@ -152,6 +152,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	function init() {
 	    var ifrm = document.createElement('iframe');
 	    ifrm.id = IFRAME_ID;
+	    ifrm.style.cssText = 'position:absolute;top:-1000px;left:1000px';
 	    document.body.appendChild(ifrm);
 	}
 
