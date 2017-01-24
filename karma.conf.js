8f229d72274c610b6b795374ea3354ab87081862
customcommander <hello@spinjs.com>
2017-01-24 09:57:33 +0000

    Makefile targets maintenance
    1.  Separate `build` from `test`
        build only generates the covered bundle now
    
    2.  As a result, Travis needs to explicitly run `make lint test`
        for validating pull requests
    
    3.  Removed Travis switches out of karma configuration and put them
        in the Makefile
    
    4.  Since you can only run karma through Make, there's no point
        in communicating paths through environment variables.
        Deleted those and hardcoded the path in
        the karma config file directly

--- karma.conf.js
+++ karma.conf.js
@@ -1,41 +1,22 @@
-// These environment variable should be defined in the Makefile
-// or passed onto the command line variable.
-var bundle = process.env.BUNDLE;
-var tests = process.env.TEST_FILES;
-var coverageDir = process.env.COV_DIR;
-
 module.exports = function(config) {
-    if (!bundle) {
-        throw new Error('BUNDLE is not defined');
-    }
-
-    if (!tests) {
-        throw new Error('TEST_FILES is not defined');
-    }
-
-    if (!coverageDir) {
-        throw new Error('COV_DIR is not defined');
-    }
-
-    var files = [].concat(bundle.split(' '), tests.split(' '));
-
     config.set({
         basePath: './',
         autoWatch: false,
         singleRun: true,
-        logLevel: config.LOG_ERROR,
         frameworks: ['mocha', 'chai'],
-        browsers: process.env.TRAVIS ? ['Chrome_Travis'] : ['Chrome'],
         customLaunchers: {
             Chrome_Travis: {
                 base: 'Chrome',
                 flags: ['--no-sandbox']
             }
         },
-        files: files,
+        files: [
+            'build/macgyver.js',
+            'test/macgyver.spec.js'
+        ],
         reporters: ['coverage', 'mocha'],
         coverageReporter: {
-            dir: coverageDir,
+            dir: 'build/test',
             reporters: [
                 {type: 'json', subdir: '.'}
             ]
